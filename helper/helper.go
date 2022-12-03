b12975daad9f900f0d29658ceb695a9e4eae8e76
Philip Vejre <philip@vejre.blue>
2022-12-03 16:52:01 +0100

    Add helper module

---
+++ helper/helper.go
@@ -0,0 +1,21 @@
+package helper
+
+import (
+	"bufio"
+	"os"
+)
+
+func ForEachLine(path string, do func(string) error) error {
+	file, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		if err := do(scanner.Text()); err != nil {
+			return err
+		}
+	}
+	return scanner.Err()
+}
